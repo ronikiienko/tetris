0f1c7d9c16ceec0a622d2294f3418aec561a39ed
Rostyslav Onikiienko <ronikiienko@gmail.com>
2023-01-19 10:46:10 +0200

    b.

--- src/Tile.js
+++ src/Tile.js
@@ -42,7 +42,7 @@ const defaultPositionTetrominoes = {
 
 const getRandomTetromino = () => {
     const tetrominoesTypes = Object.keys(defaultPositionTetrominoes);
-    const tetrominoName = tetrominoesTypes[getRandomNumberInRange(3, 3)];
+    const tetrominoName = tetrominoesTypes[getRandomNumberInRange(0, 6)];
     const tetromino = defaultPositionTetrominoes[tetrominoName];
     return {tetromino, tetrominoName};
 };
